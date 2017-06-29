91bf744aa4673443cb323499af80f4dff6a6da75
fightingchange <865636886@qq.com>
2017-06-29 09:05:20 +0800

    first commit

---
+++ src/FirstServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace One\Web;
+
+use Illuminate\Support\ServiceProvider;
+
+class FirstServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
